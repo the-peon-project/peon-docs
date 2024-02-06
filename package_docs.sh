81c29fc646ab51483d093072ebb55c11ffe373f0
Umlatt <theplacetosendmemail@gmail.com>
2024-02-06 12:28:13 +0000

    Auto output to webstie folder

--- package_docs.sh
+++ package_docs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ./manual/.
-docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material build
+docker run --rm -it -v ${PWD}:/docs -v /root/peon-project/the-peon-project.github.io:/docs/site squidfunk/mkdocs-material build
