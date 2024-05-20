d3ff01ab0e50ae3eca2dd1c074df81d7dab80a4d
umlatt <theplacetosendmemail@gmail.com>
2024-05-20 17:09:29 +0000

    Redirected for new path

--- package_docs.sh
+++ package_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-output_dir="/root/peon-project/the-peon-project.github.io"
+output_dir="/root/development/the-peon-project.github.io"
 cd ./manual/.
 docker run --rm -it -v ${PWD}:/docs -v $output_dir:/docs/site squidfunk/mkdocs-material build
 cp ./site/CNAME $output_dir/.
